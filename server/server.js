dbd61612772645772ed1c313868d9f90a3e2f7cc
NGYL <nyl162@hotmail.com>
2018-11-05 14:38:03 +0800

    bug fix

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket) {
 socket.on('message', function(data) {
-    console.log(data);
+  //  console.log(data);
     socket.broadcast.emit('message', {
-        x: data.x,
-        y: data.y,
+        x0: data.x0,
+        y0: data.y0,
+        x1: data.x1,
+        y1: data.y1,
         type: data.type
     });
 });
